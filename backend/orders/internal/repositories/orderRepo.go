68fdcf63dd89e5b1a2cc1c14bba4f2c475b62a72
zhosyaaa <musabecova05@gmail.com>
2023-09-01 16:07:33 +0600

    .

--- backend/orders/internal/repositories/orderRepo.go
+++ backend/orders/internal/repositories/orderRepo.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/zhosyaaa/foodDeliverySystems-order/internal/models"
 	"github.com/zhosyaaa/foodDeliverySystems-order/internal/repositories/interfaces"
 	"gorm.io/gorm"
 )
@@ -9,6 +10,39 @@ type orderService struct {
 	DB *gorm.DB
 }
 
-func NewOrderService(DB *gorm.DB) interfaces.OrderRepository {
-	return &orderDatabase{DB: DB}
+func newOrderService(DB *gorm.DB) interfaces.OrderRepository {
+	return &orderService{DB: DB}
+}
+
+func (o orderService) CreateOrder(order *models.Order) error {
+	if err := o.DB.Create(order).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o orderService) CancelOrder(order *models.Order) error {
+	if err := o.DB.Model(order).Update("Status", "Canceled").Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o orderService) GetUserOrders(userId uint64) ([]models.Order, error) {
+	var orders []models.Order
+	if err := o.DB.Where("customer_id = ?", userId).Find(&orders).Error; err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
+func (o orderService) GetOrderDetails(orderId uint64) (*models.Order, error) {
+	order := &models.Order{}
+	if err := o.DB.Preload("Items").First(order, orderId).Error; err != nil {
+		return nil, err
+	}
+
+	return order, nil
 }
